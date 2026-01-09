1bde1d83c952ef5972efdad6cee7a2d518f94ca8
Chandrakant <chandrakantjadhav.qa@gmail.com>
2026-01-09 16:46:45 +0530

    Dyanamic Dropdown

---
+++ src/main/java/BasicSelenium/DyanamicDropdown.java
@@ -0,0 +1,30 @@
+package BasicSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DyanamicDropdown {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		//a[@value='MAA'] - 
+		//a[@value='BLR']
+		
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		//driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		
+		//without indexing it will select first matching element only
+		//trying without index
+		//div [@id="glsctl00_mainContent_ddl_destinationStation1_CTNR"] //a[@value='MAA']
+		
+		driver.findElement(By.xpath("//div [@id=\"glsctl00_mainContent_ddl_destinationStation1_CTNR\"] //a[@value='MAA']")).click();
+
+	}
+
+}
