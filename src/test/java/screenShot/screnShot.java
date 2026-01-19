5dfc4a72bad9fed58930b8b12b47eec445ff3f8b
Chandrakant <chandrakantjadhav.qa@gmail.com>
2026-01-19 16:16:05 +0530

    optimized screenshot

--- src/test/java/screenShot/screnShot.java
+++ src/test/java/screenShot/screnShot.java
@@ -9,16 +9,18 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-public class screnShot{
-public static void main(String[] args) throws IOException {
-	// TODO Auto-generated method stub
-	WebDriver driver=new ChromeDriver();
-	driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
 
-     File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-     FileUtils.copyFile(src,new File("C:\\Users\\Abcom\\eclipse-workspace\\QA_RahulShetty_Intro_Selenium\\target\\screenshots\\screenshot1.png"));
-     
-}
+public class screnShot {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File(
+				"C:\\Users\\Abcom\\eclipse-workspace\\QA_RahulShetty_Intro_Selenium\\target\\screenshots\\screenshot1.png"));
+
+	}
 }
